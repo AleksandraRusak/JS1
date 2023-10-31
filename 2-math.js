05e487e7f6146f291cc4fdf01a7dba7807b81878
AleksandraRusak <rusakaleksandra94@gmail.com>
2023-10-31 14:51:31 +0100

    done

---
+++ 2-math.js
@@ -0,0 +1,24 @@
+// JavaScript supports the following math operators:
+//
+//   Add: +
+//   Subtract: -
+//   Multiply: *
+//   Divide: /
+//
+// For each task below, add a line beneath the task with your answer.
+
+// Task 1: Inside of a console.log(), add 3.5 to your age:
+const age = 29;
+console.log(age + 3.5);
+
+// Task 2: Inside of a console.log(), take the current year and subtract 1969:
+const year = 2023;
+console.log(year - 1969);
+
+// Task 3: Inside of a  console.log(), divide 65 by 240:
+
+console.log(65 / 240);
+
+// Task 4: Inside of a  console.log(), multiply 0.2708 by 100:
+
+console.log(0.2708 * 100);
